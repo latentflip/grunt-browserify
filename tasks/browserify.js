646229830127f69b39e346fc353987e8dc45a652
Philip Roberts <phil@latentflip.com>
2013-06-10 18:59:59 +0200

    Update browserify.js

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -13,20 +13,46 @@ var path = require('path');
 var browserify = require('browserify');
 var shim = require('browserify-shim');
 
+
 module.exports = function (grunt) {
+
   grunt.registerMultiTask('browserify', 'Grunt task for browserify.', function () {
+    var cache = {};
+    var watching = {};
+    var b;
+    var pending = false;
+
     var opts = this.options();
 
-    grunt.util.async.forEachSeries(this.files, function (file, next) {
+    grunt.event.on('watch', function(action, filepath) {
+      filepath = path.resolve(filepath)
+      grunt.log.ok(filepath)
 
+      delete cache[filepath]
+      watching[filepath] = false
+
+      if (!pending) setTimeout(function() {
+        pending = false;
+        b.emit('update');
+      }, opts.delay || 300);
+
+      pending = true;
+    });
+
+
+    grunt.util.async.forEachSeries(this.files, function (file, next) {
+      if (!file) return false
+      
       var files = grunt.file.expand({filter: 'isFile'}, file.src).map(function (f) {
         return path.resolve(f);
       });
 
-      var b = browserify(files);
+      b = browserify(files);
+
       b.on('error', function (err) {
         grunt.fail.warn(err);
       });
+
 
       if (opts.ignore) {
         grunt.file.expand({filter: 'isFile'}, opts.ignore)
@@ -91,16 +117,45 @@ module.exports = function (grunt) {
         grunt.file.mkdir(destPath);
       }
 
-      b.bundle(opts, function (err, src) {
-        if (err) {
-          grunt.fail.warn(err);
-        }
 
-        grunt.file.write(file.dest, src);
-        grunt.log.ok('Bundled ' + file.dest);
-        next();
+      var bundle = b.bundle.bind(b);
+      var first = true;
+
+      b.on('dep', function(dep) {
+        if (watching[dep.id]) return;
+        watching[dep.id] = true;
+        cache[dep.id] = dep;
       });
 
+      b.bundle = function(opts_, cb) {
+        if (b._pending) return bundle(opts_, cb);
+
+        if (typeof opts_ === 'function') {
+          cb = opts_;
+          opts_= {};
+        }
+
+        if (!opts_) opts_ = {};
+        if (!first) opts_.cache = cache;
+        first = false;
+
+        return bundle(opts_, cb);
+      };
+
+      var writeBundle = function (cb) {
+        b.bundle(opts, function(err, src) {
+          if (err) {
+            grunt.fail.warn(err);
+          }
+          grunt.file.write(file.dest, src);
+          grunt.log.ok('Bundled ' + file.dest);
+
+          if(cb) cb()
+        });
+      }
+
+      b.on('update', writeBundle)
+      writeBundle(next)
     }, this.async());
   });
 };
